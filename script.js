12cbd8cca239f0aaf2fb4b8f78c14c31e285fe31
Artyom Tratsiuk <inqui05@gmail.com>
2020-11-20 19:12:18 +0300

    fixed the homework lesson2

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ for (let i = 0; i < numString.length; i++) {
     result *= +( numString[i] );
 }
 
-let power3 = result;
+/*let power3 = result;
 
 for (let i = 1; i < 3; i++) {
     power3 *= result;
-}
+}*/
+result = String(result ** 3);
 
-power3 = String( power3 );
-alert(+(power3.substr(0,2)));
+//power3 = String( power3 );
+alert(+(result.substr(0,2)));
 
 
 
