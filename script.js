ac7079e96c5029bc64d84b30767f061658b63a34
Artyom Tratsiuk <inqui05@gmail.com>
2020-11-23 16:23:58 +0300

    extra homework to lesson03

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-let num = 266219;
+/*В задание не было указано, что необходимо производить проверку входных данных, 
+поэтому по умолчанию подразумевается, что пользователь вводит данные в нужном формате*/
+let lang = confirm( 'Вы согласны установить русский язык? При отмене будет выставлен английский!' );
+let namePerson = prompt( 'Введите имя', 'Артем' );
+let arr = [
+    ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    ['Monday', "Tuesday", 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sanday'],
+];
 
-let numString = String( num );
-let result = 1; 
-
-for (let i = 0; i < numString.length; i++) {
-    result *= +( numString[i] );
+//пункт 1 через if
+if (lang){
+    console.log( 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье' );
+} else {
+    console.log( 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sanday');
 }
 
-/*let power3 = result;
+//пункт 1 через switch-case
+switch(lang){
+    case true: {
+        console.log( 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье' );
+        break;
+    }
+    case false: {
+        console.log( 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sanday');
+        break;
+    }
+}
 
-for (let i = 1; i < 3; i++) {
-    power3 *= result;
-}*/
-result = String(result ** 3);
+//пункт 1 через многомерный массив и без if/switch
+lang ? console.log(arr[0].join(', ')) : console.log(arr[1].join(', '));
 
-//power3 = String( power3 );
-alert(+(result.substr(0,2)));
-
-
-
-
+//пункт 2
+(namePerson === 'Артем') ? console.log( 'директор' ) : 
+    (namePerson === 'Максим') ? console.log( 'преподаватель' ) : console.log( 'студент' );
