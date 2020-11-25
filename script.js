aeab336752666edfce1b7d3bc2a98c45b9f54f4f
Artyom Tratsiuk <inqui05@gmail.com>
2020-11-25 13:02:31 +0300

    homework to lesson05

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
 'use strict';
-/*Я бы мог сделать все одной большой функцией (вроде так в задании). Но раз лучше сразу 
-делать правильно, то как я понял из теории - одна функция должна делать только одну работу*/
-let data = '    It is a string! Ahahaha hohoho hihihi   ',
-    maxCharacters = 30;
+let arr = ['1532', '28462.2', '956.2848', '4542', '632.1', '9595.25', '444.444'],
+    maxNumber = 100;
 
-function isString( data ){
-    if ( typeof data !== 'string' ){
-        return false;
-    } else {
+function isWantedNumbers( str ){
+    if ( +str.substring( 0, 1 ) === 2 || +str.substring( 0, 1 ) === 4){
         return true;
-    }
-}
-
-function fixTheString( data ){
-    data = data.trim();
-    if ( data.length > maxCharacters ){
-        return data.substring(0, maxCharacters) + '...' ;
     } else {
-        return data;
+        return false;
     }
 }
 
-function checkAndFix(data){
-    if (isString(data)){
-        return fixTheString(data);
-    } else {
-        return 'Unfortunately, your data is not a string!';
+function findNumbers(anyArr){
+    let trueNumbersArr = [];
+    for( let i = 0; i < anyArr.length ; i++ ){
+        if ( isWantedNumbers( anyArr[i] ) ){
+            trueNumbersArr.push( anyArr[i] );
+        }
+    }
+    return trueNumbersArr;
+}
+
+/*Вообще, я и саv не знаю, нужна ли тут функция. Не знаю, насколько это важно, но решил написать
+ чистую функцию выше (хотя можно туда запихнуть console.log и не дурить голову).
+ Правда тут все равно уперся в то, что так не получится. Буду рад комментарию, что делать в этом случае*/
+function printNumbers(anyArr){
+    for( let i = 0; i < anyArr.length ; i++){
+        console.log( anyArr[i] );
     }
 }
 
-console.log( checkAndFix( data) );
+//the second part of homework
+function isPrimeNumber(number){
+    for ( let i = 2; i < number; i++){
+        if (number % i === 0 && number !== 2){
+            return false;
+        }
+    }
+    return true;
+}
+
+function printPrimeNumbers(number){
+    for ( let i = 2; i < number; i++){
+        if (isPrimeNumber(i)){
+            console.log(i + ` - делители этого числа ${1} и ${i}`);
+        }
+    }
+}
+
+printNumbers( findNumbers(arr) );
+printPrimeNumbers(maxNumber);
