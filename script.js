4bf07d28c3ee6ef121e5ada64fb843f6a6974b0b
Artyom Tratsiuk <inqui05@gmail.com>
2020-11-28 02:38:38 +0300

    the first edition of my homework07

--- script.js
+++ script.js
@@ -1,47 +1,27 @@
 'use strict';
-let text = 'Угадай число от 1 до 100';
+let daysOfTheWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
+    currentDay = new Date();
+    currentDay = currentDay.getDay();
 
-function startGame(){
-    let number = Math.floor(Math.random() * 100) + 1,
-        attempts = 10;
-    askPersonNumber(text, number, attempts);
-}
-
-function isNumber(data){
-    return !isNaN(parseFloat(data)) && isFinite(data);
-}
-
-function askPersonNumber(text, number, attempts){
-    if (attempts === 0){
-        if (confirm('Попытки закончились, хотите сыграть еще?')) {
-            startGame();
+    function checkTheDays(arr, index, day){
+        if ((index + 1) === day && (arr[index] === "Суббота" || arr[index] === "Воскресенье")){
+            return arr[index].bold().italics();
+        } else if (arr[index] === "Суббота" || arr[index] === "Воскресенье"){
+            return arr[index].italics();
         } else {
-            alert('Спасибо за игру!');
+            return arr[index];
         }
-    } else {
-        let ask = prompt(text); 
-        console.dir(ask);
+    }
 
-        if (ask === null){
-            alert('Игра окончена!');
-        } else if (!isNumber(ask)){
-            askPersonNumber('Введи число!', number, attempts);
-        } else {
-              if(+ask === number){
-                if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')){
-                    startGame();
-                }
-            } else if (+ask > number){
-                attempts--;
-                alert(`Загаданное число меньше, осталось ${attempts} попыток...`);
-                askPersonNumber('Пробуй! Напоминаю, от 1 до 100:', number, attempts);
-            } else {
-                attempts--;
-                alert(`Загаданное число больше, осталось ${attempts} попыток...`);
-                askPersonNumber('Пробуй! Напоминаю, от 1 до 100:', number, attempts);
-            }
+    function print(arr){
+        let finalResult = null;
+        for (let i = 0; i < arr.length; i++){
+            finalResult += (checkTheDays(daysOfTheWeek, i, currentDay)) + "\n";
         }
-    }  
-}
+        alert(finalResult);
+    }
 
-startGame();
+    print(daysOfTheWeek);
+
+    /*жирный .bold() курсив .italics()
+    */
