a3d9f5a3c8bafa3805bb3eb8cf61285fc40a1efa
Artyom Tratsiuk <inqui05@gmail.com>
2020-12-02 10:15:20 +0300

    my homework to lesson09

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
 'use strict';
 let daysOfTheWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
-    currentDay = new Date();
-    currentDay = currentDay.getDay();
+    months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", 
+    "Октября", "Ноября", "Декабря"];
+    //массив используется для проверки окончания Ы при выводе минут и секунд
+    let endingsInTheWord1 = [2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54];
+    //массив используется для проверки окончания А при выводе минут и секунд
+    let endingsInTheWord2 = [1, 21, 31, 41, 51];
 
-    function checkTheDays(arr, index, day){
-        if ((index + 1) === day && (arr[index] === "Суббота" || arr[index] === "Воскресенье")){
-            return arr[index].bold().italics();
-        } else if (arr[index] === "Суббота" || arr[index] === "Воскресенье"){
-            return arr[index].italics();
+    function checkHours(number){
+        if (number === 1 || number === 21){
+            return ' час ';
+        } else if (number === 2 || number === 3 || number === 4 || number === 22  || number === 23){
+            return ' часа ';
         } else {
-            return arr[index];
+            return ' часов ';
         }
     }
 
-    function print(arr){
-        for (let i = 0; i < arr.length; i++){
-            document.write(checkTheDays(daysOfTheWeek, i, currentDay)  + "<br>") ;
+    function checkMinutes(number){
+        if (endingsInTheWord1.indexOf(number) !== -1){
+            return ' минуты ';
+        } else if (endingsInTheWord2.indexOf(number) !== -1){
+            return ' минута ';
+        } else {
+            return ' минут ';
         }
     }
 
-    print(daysOfTheWeek);
+    function checkSeconds(number){
+        if (endingsInTheWord1.indexOf(number) !== -1){
+            return ' секунды';
+        } else if (endingsInTheWord2.indexOf(number) !== -1){
+            return ' секунда';
+        } else {
+            return ' секунд';
+        }
+    }
+
+    function addZero(number){
+        if (number < 10){
+            return '0' + number.toString();
+        } else {
+            return number;
+        }
+    }
+
+    function printTheFirstWay(){
+        let date = new Date();
+        document.write(`Сегодня ${daysOfTheWeek[date.getDay() - 1]}, ${date.getDate()} 
+        ${months[date.getMonth()]} ${date.getFullYear()} года, ${date.getHours()} 
+        ${checkHours(date.getHours())} ${date.getMinutes()} ${checkMinutes(date.getMinutes())}
+         ${date.getSeconds()} ${checkSeconds(date.getSeconds())}<br/>`);
+    }
+
+    function printTheSecondWay(){
+        let date = new Date();
+        document.write(`${addZero(date.getDate())}.${addZero(date.getMonth() + 1)}.
+        ${date.getFullYear()} - ${addZero(date.getHours())}:${addZero(date.getHours())}
+        :${addZero(date.getSeconds())}<br/>`);
+    }
+
+    setInterval(printTheFirstWay, 1000);
+    setInterval(printTheSecondWay, 1000);
