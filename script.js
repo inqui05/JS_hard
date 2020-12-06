fe1b11e24cb764e10aa31c187c3c273444dfac22
Artsiom Tratsiuk <inqui05@gmail.com>
2020-12-06 20:31:44 +0300

    my homework to lesson12

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
 'use strict';
-let daysOfTheWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
-    months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", 
-    "Октября", "Ноября", "Декабря"];
-    //массив используется для проверки окончания Ы при выводе минут и секунд
-    let endingsInTheWord1 = [2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54];
-    //массив используется для проверки окончания А при выводе минут и секунд
-    let endingsInTheWord2 = [1, 21, 31, 41, 51];
 
-    function checkHours(number){
-        if (number === 1 || number === 21){
-            return ' час ';
-        } else if (number === 2 || number === 3 || number === 4 || number === 22  || number === 23){
-            return ' часа ';
-        } else {
-            return ' часов ';
+const todoControl = document.querySelector('.todo-control'), //
+    headerInput = document.querySelector('.header-input'),  //ввод задания
+    todoList = document.querySelector('.todo-list'), //не выполненое задание
+    todoCompleted = document.querySelector('.todo-completed'); //выполнено ли задание
+
+    const todoData = [
+
+    ];
+
+    const render = function(){
+        if (todoData.length === 0){
+            JSON.parse(localStorage.getItem('todo')).forEach(function(value){
+                todoData.push(value);
+            });
         }
-    }
+        todoList.textContent = '';
+        todoCompleted.textContent = '';
 
-    function checkMinutes(number){
-        if (endingsInTheWord1.indexOf(number) !== -1){
-            return ' минуты ';
-        } else if (endingsInTheWord2.indexOf(number) !== -1){
-            return ' минута ';
-        } else {
-            return ' минут ';
+        todoData.forEach(function(item){
+            const li = document.createElement('li');
+            li.classList.add('todo-item');
+
+            li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+             '<div class="todo-buttons">' +
+             '<button class="todo-remove"></button>' +
+             '<button class="todo-complete"></button>' +
+             '</div>';
+
+            if (item.completed){
+                todoCompleted.append(li);
+            } else {
+                todoList.append(li);
+            }
+
+            const btnTodoComplete = li.querySelector('.todo-complete'); //кнопка задание выполнено
+            btnTodoComplete.addEventListener('click', function(){
+                item.completed = !item.completed;
+                render();
+            });
+
+            const btnTodoRemove = li.querySelector('.todo-remove');
+            btnTodoRemove.addEventListener('click', function(){
+                todoData.forEach(function(value, i){
+                    if (value.value === item.value){
+                        todoData.splice(todoData[i], 1);
+                        localStorage.todo = JSON.stringify(todoData);
+                    }
+                });
+                render();
+            });
+        });
+    };
+
+    todoControl.addEventListener('submit', function(event){
+        event.preventDefault();
+
+        const newTodo = {
+            value: headerInput.value,
+            completed: false
+        };
+        
+        if(newTodo.value !== ''){
+            todoData.push(newTodo);
+            localStorage.todo = JSON.stringify(todoData);
+            headerInput.value = '';
         }
-    }
 
-    function checkSeconds(number){
-        if (endingsInTheWord1.indexOf(number) !== -1){
-            return ' секунды';
-        } else if (endingsInTheWord2.indexOf(number) !== -1){
-            return ' секунда';
-        } else {
-            return ' секунд';
-        }
-    }
+        render();
+    });
 
-    function addZero(number){
-        if (number < 10){
-            return '0' + number.toString();
-        } else {
-            return number;
-        }
-    }
-
-    function printTheFirstWay(){
-        let date = new Date();
-        document.write(`Сегодня ${daysOfTheWeek[date.getDay() - 1]}, ${date.getDate()} 
-        ${months[date.getMonth()]} ${date.getFullYear()} года, ${date.getHours()} 
-        ${checkHours(date.getHours())} ${date.getMinutes()} ${checkMinutes(date.getMinutes())}
-         ${date.getSeconds()} ${checkSeconds(date.getSeconds())}<br/>`);
-    }
-
-    function printTheSecondWay(){
-        let date = new Date();
-        document.write(`${addZero(date.getDate())}.${addZero(date.getMonth() + 1)}.
-        ${date.getFullYear()} - ${addZero(date.getHours())}:${addZero(date.getMinutes())}
-        :${addZero(date.getSeconds())}<br/>`);
-    }
-
-    setInterval(printTheFirstWay, 1000);
-    setInterval(printTheSecondWay, 1000);
+    render();
