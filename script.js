e131b503e3bbf9e701f5bd9d454e874ef584acd9
Artsiom Tratsiuk <inqui05@gmail.com>
2020-12-15 23:02:16 +0300

    my homework to lesson18

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
 'use strict';
-let daysOfTheWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
-    months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", 
-    "Октября", "Ноября", "Декабря"];
-    //массив используется для проверки окончания Ы при выводе минут и секунд
-    let endingsInTheWord1 = [2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54];
-    //массив используется для проверки окончания А при выводе минут и секунд
-    let endingsInTheWord2 = [1, 21, 31, 41, 51];
+const daysOfTheWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
+    timeOfDay = ['Доброе утро!', 'Добрый день!', 'Добрый вечер!', 'Доброй ночи!'],
+    h1 = document.querySelector('h1');
+let date = new Date();
 
-    function checkHours(number){
-        if (number === 1 || number === 21){
-            return ' час ';
-        } else if (number === 2 || number === 3 || number === 4 || number === 22  || number === 23){
-            return ' часа ';
-        } else {
-            return ' часов ';
-        }
+function addZero(number){
+    if (number < 10){
+        return '0' + number.toString();
+    } else {
+        return number;
     }
+}
 
-    function checkMinutes(number){
-        if (endingsInTheWord1.indexOf(number) !== -1){
-            return ' минуты ';
-        } else if (endingsInTheWord2.indexOf(number) !== -1){
-            return ' минута ';
-        } else {
-            return ' минут ';
-        }
+function addDay(){
+    let day = document.createElement("p");
+    if (date.getHours() >= 0 && date.getHours() < 6) {
+        day.textContent = timeOfDay[3];
+    } else if (date.getHours() >= 6 && date.getHours() < 12) {
+        day.textContent = timeOfDay[0];
+    } else if (date.getHours() >= 12 && date.getHours() < 18) {
+        day.textContent = timeOfDay[1];
+    } else {
+        day.textContent = timeOfDay[2];
     }
+    h1.after(day);
+}
 
-    function checkSeconds(number){
-        if (endingsInTheWord1.indexOf(number) !== -1){
-            return ' секунды';
-        } else if (endingsInTheWord2.indexOf(number) !== -1){
-            return ' секунда';
-        } else {
-            return ' секунд';
-        }
+function addDayOfTheWeek(){
+    let day = document.createElement("p");
+    day.textContent = `Сегодня: ${daysOfTheWeek[date.getDay() - 1]}`;
+    h1.after(day);
+}
+
+function addCurrentTime(){
+    let day = document.createElement("p");
+    day.textContent = `Текущее время: ${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`;
+    h1.after(day);
+}
+
+function howLongToNY(){
+    let day = document.createElement("p");
+    const untilNY = new Date('01/01/2021').getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (untilNY - dateNow) / 1000;
+
+    let daysToNY = Math.floor(timeRemaining / 60 / 60 / 24);
+
+    if (daysToNY > 4) {
+        day.textContent = `До нового года осталось ${daysToNY} дней`;
+    } else if(daysToNY <= 4 && daysToNY > 1) {
+        day.textContent = `До нового года осталось ${daysToNY} дня`;
+    } else if(daysToNY === 1) {
+        day.textContent = `До нового года осталось ${daysToNY} день`;
+    } else {
+        day.textContent = `Уже Новый год!`;
     }
+    h1.after(day);
+}
 
-    function addZero(number){
-        if (number < 10){
-            return '0' + number.toString();
-        } else {
-            return number;
-        }
-    }
-
-    function printTheFirstWay(){
-        let date = new Date();
-        document.write(`Сегодня ${daysOfTheWeek[date.getDay() - 1]}, ${date.getDate()} 
-        ${months[date.getMonth()]} ${date.getFullYear()} года, ${date.getHours()} 
-        ${checkHours(date.getHours())} ${date.getMinutes()} ${checkMinutes(date.getMinutes())}
-         ${date.getSeconds()} ${checkSeconds(date.getSeconds())}<br/>`);
-    }
-
-    function printTheSecondWay(){
-        let date = new Date();
-        document.write(`${addZero(date.getDate())}.${addZero(date.getMonth() + 1)}.
-        ${date.getFullYear()} - ${addZero(date.getHours())}:${addZero(date.getMinutes())}
-        :${addZero(date.getSeconds())}<br/>`);
-    }
-
-    setInterval(printTheFirstWay, 1000);
-    setInterval(printTheSecondWay, 1000);
+    howLongToNY();
+    addCurrentTime();
+    addDayOfTheWeek();
+    addDay();
